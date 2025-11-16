b097048a6f330bcc46f528b133e078f29fda663a
Matheus Boscariol <moboscariol2010@gmail.com>
2025-11-16 12:26:24 -0500

    Initial Commit

---
+++ MomentumFitness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MomentumFitness
+//
+//  Created by Ana Paula De Oliveira on 2025-11-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MomentumFitness/MomentumFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumFitnessApp.swift
+//  MomentumFitness
+//
+//  Created by Ana Paula De Oliveira on 2025-11-16.
+//
+
+import SwiftUI
+
+@main
+struct MomentumFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
