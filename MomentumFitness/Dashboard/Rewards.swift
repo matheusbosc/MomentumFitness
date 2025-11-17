1861cd9e2255ea7ad4b6c2f71f877cc9ebb89357
Matheus Boscariol <59032664+matheusbosc@users.noreply.github.com>
2025-11-16 20:50:12 -0500

    Added base elements to RewardsView.swift

--- MomentumFitness/Dashboard/Rewards.swift
+++ MomentumFitness/Dashboard/Rewards.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct RewardsView: View {
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+    
+    let mainGradient = LinearGradient(colors: [
+        Color(red: 0.22, green: 0.51, blue: 0.843),
+        Color(red: 0.059, green: 0.8, blue: 0.565)],
+                                      startPoint: .leading,
+                                      endPoint: .trailing)
+    
     var body: some View {
-        Text("Rewards")
+        VStack{
+            Spacer(minLength: 35)
+            Text("rewards")
+                .font(.custom("Quicksand", size: 24))
+        }.background(.clear)
     }
 }
 
