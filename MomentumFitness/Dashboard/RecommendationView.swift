a2e2d9697352132a14325a28f89b5d8eada77402
Matheus Boscariol <59032664+matheusbosc@users.noreply.github.com>
2025-11-16 20:49:45 -0500

    Added base elements to RecommendationView.swift

--- MomentumFitness/Dashboard/RecommendationView.swift
+++ MomentumFitness/Dashboard/RecommendationView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct RecommendationView: View {
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+    
+    let mainGradient = LinearGradient(colors: [
+        Color(red: 0.22, green: 0.51, blue: 0.843),
+        Color(red: 0.059, green: 0.8, blue: 0.565)],
+                                      startPoint: .leading,
+                                      endPoint: .trailing)
+    
     var body: some View {
-        Text("Recommendation")
+        VStack{
+            Spacer(minLength: 35)
+            Text("recommendation")
+                .font(.custom("Quicksand", size: 24))
+        }.background(.clear)
     }
 }
 
