7d78e4ea00b03e0ca835a505bf3eb95cc320d037
Matheus Boscariol <59032664+matheusbosc@users.noreply.github.com>
2025-11-20 15:08:33 +0000

    aaaaaaaa

--- MomentumFitness/MomentumFitnessApp.swift
+++ MomentumFitness/MomentumFitnessApp.swift
@@ -12,8 +12,21 @@ import SwiftUI
 struct MomentumFitnessApp: App {
     
     @State private var loggedIn = false
+    private var server_url: String = "192.168.2.180:2010"
     
     var body: some Scene {
+
+        let access = KeychainService.get("access_token")
+        let refresh = KeychainService.get("refresh_token")
+
+        if (access != nil && refresh != nil){
+            //let request =
+
+            if (request = ""){
+                loggedIn = true
+            } 
+        }
+
         WindowGroup {
             if loggedIn {
                             HomeView()
