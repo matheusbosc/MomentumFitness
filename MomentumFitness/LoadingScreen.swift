bc0ab268d210a9108e8a60026d42b0bd78cf58e0
Matheus Boscariol <moboscariol2010@gmail.com>
2025-12-01 21:04:52 -0500

    gghjhgjgyu

---
+++ MomentumFitness/LoadingScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingScreen.swift
+//  MomentumFitness
+//
+//  Created by Ana Paula De Oliveira on 2025-11-28.
+//
+
+import SwiftUI
+
+struct LoadingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingScreen()
+}
---
+++ MomentumFitness/NavigationMenu.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationMenu.swift
+//  MomentumFitness
+//
+//  Created by Ana Paula De Oliveira on 2025-11-28.
+//
+
+import SwiftUI
+
+struct NavigationMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationMenu()
+}
